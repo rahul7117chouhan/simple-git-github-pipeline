2292fb917164f5eb1fabc3802d060e21928dad02
Rahul Chouhan <rahul7117chouhan@gmail.com>
2025-11-21 13:39:00 +0530

    Updated Code

--- app.py
+++ app.py
@@ -2,3 +2,8 @@ def greet():
     return "Hello Devops!"
 
 print (greet())
+
+def greet1():
+    return "Hello Devops New!"
+
+print (greet1())
--- test_app.py
+++ test_app.py
@@ -2,3 +2,8 @@ from app import greet
 
 def test_greet():
     assert greet() == "Hello Devops!"
+
+from app import greet1
+
+def test_greet():
+    assert greet1() == "Hello Devops New!"
